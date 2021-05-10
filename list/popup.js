5ef35fdd55c77ed28faffec51bb5f49a2739d29f
Laimonas <laimonas.milasius@latgrupe.lt>
2021-05-10 12:22:30 +0300

    Update added "pertraukos"

--- list/popup.js
+++ list/popup.js
@@ -1,20 +1,39 @@
 const date = new Date();
+const brakeColor = '#ffee11';
+const lessonColor = '#F8DAEE';
 //date.setHours(9, 57, 0, 0);
 const minutes = date.getMinutes() + (date.getHours() * 60);
+
 if(minutes >= (8 * 60) + 0 && minutes < (8 * 60) + 45){
-  document.getElementById("p_1").style.background = "red";
+  document.getElementById("p_1").style.background = lessonColor;
+} else if(minutes >= (8 * 60) + 45 && minutes < (8 * 60) + 55){
+  document.getElementById("p_1.1").style.background = brakeColor;
 } else if(minutes >= (8 * 60) + 55 && minutes < (9 * 60) + 40){
-  document.getElementById("p_2").style.background = "red";
+  document.getElementById("p_2").style.background = lessonColor;
+} else if(minutes >= (9 * 60) + 40 && minutes < (9 * 60) + 50){
+  document.getElementById("p_2.1").style.background = brakeColor;
 } else if(minutes >= (9 * 60) + 50 && minutes < (10 * 60) + 35){
-  document.getElementById("p_3").style.background = "red";
+  document.getElementById("p_3").style.background = lessonColor;
+} else if(minutes >= (10 * 60) + 35 && minutes < (10 * 60) + 55){
+  document.getElementById("p_3.1").style.background = brakeColor;
 } else if(minutes >= (10 * 60) + 55 && minutes < (11 * 60) + 40){
-  document.getElementById("p_4").style.background = "red";
+  document.getElementById("p_4").style.background = lessonColor;
+} else if(minutes >= (11 * 60) + 40 && minutes < (12 * 60) + 0){
+  document.getElementById("p_4.1").style.background = brakeColor;
 } else if(minutes >= (12 * 60) + 0 && minutes < (12 * 60) + 45){
-  document.getElementById("p_5").style.background = "red";
+  document.getElementById("p_5").style.background = lessonColor;
+} else if(minutes >= (12 * 60) + 45 && minutes < (12 * 60) + 55){
+  document.getElementById("p_5.1").style.background = brakeColor;
 } else if(minutes >= (12 * 60) + 55 && minutes < (13 * 60) + 40){
-  document.getElementById("p_6").style.background = "red";
+  document.getElementById("p_6").style.background = lessonColor;
+} else if(minutes >= (13 * 60) + 40 && minutes < (13 * 60) + 50){
+  document.getElementById("p_6.1").style.background = brakeColor;
 } else if(minutes >= (13 * 60) + 50 && minutes < (14 * 60) + 35){
-  document.getElementById("p_7").style.background = "red";
+  document.getElementById("p_7").style.background = lessonColor;
+} else if(minutes >= (13 * 60) + 35 && minutes < (14 * 60) + 45){
+  document.getElementById("p_7.1").style.background = brakeColor;
 } else if(minutes >= (14 * 60) + 45 && minutes < (15 * 60) + 30){
-  document.getElementById("p_8").style.background = "red";
+  document.getElementById("p_8").style.background = lessonColor;
+} else if(minutes >= (15 * 60) + 30){
+  document.getElementById("p_8.1").style.background = brakeColor;
 }
